c9416f268f9bd926e99174adbd60e968fb6f9f68
GabryDX <CORSO 4@192.168.104.13>
2018-02-14 17:16:48 +0100

    Second commit

--- mvc/src/main/java/it/corso/mvc/universita/model/Studente.java
+++ mvc/src/main/java/it/corso/mvc/universita/model/Studente.java
@@ -20,9 +20,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
 
 /**
  * The persistent class for the studenti database table.
